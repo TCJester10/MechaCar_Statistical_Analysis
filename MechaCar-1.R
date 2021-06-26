dbba87fa991323539d8bd3ed750a66caef3e5b0c
unknown <tessac1995@gmail.com>
2021-06-26 18:12:06 -0500

    D2

--- MechaCar-1.R
+++ MechaCar-1.R
@@ -4,8 +4,18 @@ library(dplyr)
 ## read csv
 MechaCar <- read.csv(file="MechaCar_mpg.csv")
 
-##Set Linear model
+## Set Linear model
 LinMod <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar)
 
 ## set summary stats
 summary(LinMod)
+
+
+## read csv
+suspension_table<- read.csv(file="Suspension_Coil.csv")
+
+#set summary stats
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#set lot summary table
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
