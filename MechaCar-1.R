665e596bea32ee0f710931e36acda585c35cc489
unknown <tessac1995@gmail.com>
2021-06-27 14:39:08 -0500

    final del

--- MechaCar-1.R
+++ MechaCar-1.R
@@ -1,17 +1,17 @@
-## packages
+# packages
 library(dplyr)
 
-## read csv
+# read csv
 MechaCar <- read.csv(file="MechaCar_mpg.csv")
 
-## Set Linear model
+# Set Linear model
 LinMod <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar)
 
-## set summary stats
+# set summary stats
 summary(LinMod)
 
 
-## read csv
+# read csv
 suspension_table<- read.csv(file="Suspension_Coil.csv")
 
 #set summary stats
@@ -19,3 +19,13 @@ total_summary <- suspension_table %>% summarize(Mean=mean(PSI), Median=median(PS
 
 #set lot summary table
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#t-tests
+t.test(suspension_table$PSI,mu=1500)
+
+#PSI t.test by lot
+t.test(subset(suspension_table, Manufacturing_Lot == "Lot1")$PSI,mu=1500)
+
+t.test(subset(suspension_table, Manufacturing_Lot == "Lot2")$PSI,mu=1500)
+
+t.test(subset(suspension_table, Manufacturing_Lot == "Lot3")$PSI,mu=1500)
